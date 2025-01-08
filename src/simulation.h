e157fca35feef708749a4c0acd92e657c74df6a0
Roberto Diniz <bethodr@gmail.com>
2025-01-08 17:44:14 -0300

    Fix errors in StructPoissonSolver2D instantiation

--- src/simulation.h
+++ src/simulation.h
@@ -4,70 +4,77 @@
 #include <spark/collisions/mcc.h>
 #include <spark/particle/species.h>
 #include <spark/spatial/grid.h>
-
+#include <spark/em/poisson.h>
+#include <spark/core/matrix.h>
 #include <string>
+#include <vector>
 
 #include "events.h"
 #include "parameters.h"
 
 namespace spark {
 
-class Simulation {
-public:
-    class StateInterface {
+    class Simulation {
     public:
-        StateInterface(Simulation& sim) : sim_(sim) {}
-        const spark::spatial::UniformGrid<2>& electron_density() const { return sim_.electron_density_; }
-        const spark::spatial::UniformGrid<2>& ion_density() const { return sim_.ion_density_; }
-        const spark::particle::ChargedSpecies<2, 3>& ions() const { return sim_.ions_; }
-        const spark::particle::ChargedSpecies<2, 3>& electrons() const { return sim_.electrons_; }
+        class StateInterface {
+        public:
+            StateInterface(Simulation& sim) : sim_(sim) {}
+            const spark::spatial::UniformGrid<2>& electron_density() const { return sim_.electron_density_; }
+            const spark::spatial::UniformGrid<2>& ion_density() const { return sim_.ion_density_; }
+            const spark::particle::ChargedSpecies<2, 3>& ions() const { return sim_.ions_; }
+            const spark::particle::ChargedSpecies<2, 3>& electrons() const { return sim_.electrons_; }
 
-        const Parameters& parameters() const { return sim_.parameters_; }
+            const Parameters& parameters() const { return sim_.parameters_; }
 
-        size_t step() const { return sim_.step; }
+            size_t step() const { return sim_.step; }
+
+        private:
+            Simulation& sim_;
+        };
+
+        friend StateInterface;
+
+        explicit Simulation(const Parameters& parameters, const std::string& data_path);
+
+        void run();
+
+        enum class Event { Start, Step, End };
+
+        struct EventAction {
+            virtual void notify(const StateInterface&) = 0;
+            virtual ~EventAction() {}
+        };
+
+        Events<Event, EventAction>& events();
+        StateInterface& state() { return state_; };
 
     private:
-        Simulation& sim_;
+        Parameters parameters_;
+        std::string data_path_;
+        StateInterface state_;
+
+        size_t step = 0;
+        spark::particle::ChargedSpecies<2, 3> ions_;
+        spark::particle::ChargedSpecies<2, 3> electrons_;
+
+        spark::spatial::UniformGrid<2> electron_density_;
+        spark::spatial::UniformGrid<2> ion_density_;
+
+        spark::spatial::UniformGrid<2> rho_field_;
+        spark::spatial::UniformGrid<2> phi_field_;
+
+        Events<Event, EventAction> events_;
+            
+        std::vector<spark::em::StructPoissonSolver2D::Region> region() const;
+
+        spark::core::TMatrix<spark::core::TVec<double, 2>, 1> electron_field;
+        spark::core::TMatrix<spark::core::TVec<double, 2>, 1> ion_field;
+        spark::core::TMatrix<spark::core::TVec<double, 2>, 2> electric_field_;
+
+        void set_initial_conditions();
+        spark::collisions::MCCReactionSet<2, 3> load_electron_collisions();
+        spark::collisions::MCCReactionSet<2, 3> load_ion_collisions();
     };
+} // namespace ccp
 
-    friend StateInterface;
-
-    explicit Simulation(const Parameters& parameters, const std::string& data_path);
-
-    void run();
-
-    enum class Event { Start, Step, End };
-
-    struct EventAction {
-        virtual void notify(const StateInterface&) = 0;
-        virtual ~EventAction() {}
-    };
-
-    Events<Event, EventAction>& events();
-    StateInterface& state() { return state_; };
-
-private:
-    Parameters parameters_;
-    std::string data_path_;
-    StateInterface state_;
-
-    size_t step = 0;
-    spark::particle::ChargedSpecies<2, 3> ions_;
-    spark::particle::ChargedSpecies<2, 3> electrons_;
-
-    spark::spatial::UniformGrid<2> electron_density_;
-    spark::spatial::UniformGrid<2> ion_density_;
-
-    spark::spatial::UniformGrid<2> rho_field_;
-    spark::spatial::UniformGrid<2> phi_field_;
-    spark::spatial::UniformGrid<2> electric_field_;
-
-    Events<Event, EventAction> events_;
-
-    void set_initial_conditions();
-    spark::collisions::MCCReactionSet<2, 3> load_electron_collisions();
-    spark::collisions::MCCReactionSet<2, 3> load_ion_collisions();
-};
-}  // namespace ccp
-
-#endif  // SIMULATION_H
+#endif // SIMULATION_H
