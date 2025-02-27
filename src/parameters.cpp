905ebe5780a4be558fbbb52a95004ae92c639bd8
Roberto Diniz <bethodr@gmail.com>
2025-02-27 17:03:59 -0300

    Parameters adjustments - Vertical cells and Turner data

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -25,7 +25,7 @@ Parameters Parameters::case_1() {
     p.fixed_parameters();
 
     p.nx = 129; // number of horizontal cells
-    p.ny = 5; // number of vertical cells
+    p.ny = 4; // number of vertical cells
     p.dt = 1.0 / (400.0 * p.f); // time step (s)
     p.ng = 9.64e20; // neutral density (m^-3)
     p.n0 = 2.56e14; // plasma density (m^-3)
@@ -43,14 +43,14 @@ Parameters Parameters::case_2() {
     p.fixed_parameters();
 
     p.nx = 257;
-    p.ny = 135;
-    p.dt = 1.0 / (400.0 * p.f);
+    p.ny = 4;
+    p.dt = 1.0 / (800.0 * p.f);
     p.ng = 32.1e20;
     p.n0 = 5.12e14;
     p.volt = 200.0;
     p.ppc = 256;
-    p.n_steps = 500;
-    p.n_steps_avg = 10;
+    p.n_steps = 4'096'000;
+    p.n_steps_avg = 25'600;
 
     p.computed_parameters();
     return p;
@@ -60,15 +60,15 @@ Parameters Parameters::case_3() {
     Parameters p{};
     p.fixed_parameters();
 
-    p.nx = 100;
-    p.ny = 50;
-    p.dt = 1.0 / (400.0 * p.f);
+    p.nx = 513;
+    p.ny = 4;
+    p.dt = 1.0 / (1600.0 * p.f);
     p.ng = 96.4e20;
     p.n0 = 5.12e14;
     p.volt = 150.0;
     p.ppc = 128;
-    p.n_steps = 1'000;
-    p.n_steps_avg = 50;
+    p.n_steps = 8'192'000;
+    p.n_steps_avg = 51'200;
 
     p.computed_parameters();
     return p;
@@ -78,8 +78,8 @@ Parameters Parameters::case_4() {
     Parameters p{};
     p.fixed_parameters();
 
-    p.nx = 150;
-    p.ny = 75;
+    p.nx = 513;
+    p.ny = 4;
     p.dt = 1.0 / (3200.0 * p.f);
     p.ng = 321.0e20;
     p.n0 = 3.84e14;
